d86807d3cb303ec98f2fb9aba74077ae76eae202
amadorcarlos <carlos.amador450@gmail.com>
2017-09-18 11:04:26 -0600

    tasks.py and order files

--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.views.generic.edit import FormView
+from django.shortcuts import redirect
+
+from .forms import GenerateRandomUserForm
+from .tasks import create_random_user_accounts
+
+class  GenerateRandomUserForm(FormView):
+	template_name = 'core/generate_random_users.html'
+	form_class = GenerateRandomUserForm
+
+	def form_valid(self, form):
+		total = form.cleaned_data.get ('total')
+		create_random_user_accounts.delay(total)
+		messages.success (self.request, 'Estamos generando tu usuaruio aleatorio! Espera un momento y refresca esta pagina.')
+		return redirect ('useers_list')
+		
