830ebbaabfebfe4d76276d655699b8cfc86cc095
amadorcarlos <carlos.amador450@gmail.com>
2017-09-21 12:18:59 -0600

    frontend

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from .views import GenerateRandomUserForm
+from .views import UsersListView
 
 urlpatterns = [
     url(r'templates/$', GenerateRandomUserForm.as_view(), name='GenerateRandomUserForm'),
+    url(r'users/$', UsersListView.as_view(), name='UsersListView'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.views.generic.edit import FormView
 from django.shortcuts import redirect
+from django.views import generic
 
 from .forms import GenerateRandomUserForm
 from .tasks import create_random_user_accounts
-
+  
 class  GenerateRandomUserForm(FormView):
-	template_name = 'generate_random_users.html'
-	form_class = GenerateRandomUserForm
+        template_name = 'generate_random_users.html'
+        form_class = GenerateRandomUserForm
 
 def form_valid(self, form):
-        total = form.cleaned_data.get('total')
-        create_random_user_accounts.delay(total)
-        messages.success(self.request, 'We are generating your random users! Wait a moment and refresh this page.')
-        return redirect('users_list')
+    total = form.cleaned_data.get('total')                                                                                            
+    create_random_user_accounts.delay(total)
+    messages.success(self.request, 'We are generating your random users! Wait a moment and refresh this page.')
+    return redirect('users_list')
+
+class  UsersListView(generic.ListView):
+     template_name = 'users/generate_random_users.html'
+     model = User
+
+def get_queryset(self):
+    return User.objects.all()
